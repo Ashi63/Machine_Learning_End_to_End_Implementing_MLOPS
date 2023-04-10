313fae73e625061d21c2ee8cbda51d9008a0fa18
Ashi63 <ashish.ashi6388@gmail.com>
2023-04-10 18:14:51 +0530

    setup.py file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
-# set up file
+from setuptools import setup,find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return a list of requirements
+    '''    
+    requirements = []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [requirement.replace('\n','') for requirement in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements   
+        
+    
+    
+    
+    
+    
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Ashi63',
+    author_email='ashish.ashi6388@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
